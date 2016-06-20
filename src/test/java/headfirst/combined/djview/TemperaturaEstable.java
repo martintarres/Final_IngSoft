b910ee6646e83fe3fe321de57eefaabc8e5ea7ee
unknown <alexader.fyh@gmail.com>
2016-06-20 00:35:37 -0300

    Agregando Docs y Test a la master

---
+++ src/test/java/headfirst/combined/djview/TemperaturaEstable.java
@@ -0,0 +1,29 @@
+package headfirst.combined.djview;
+import com.sun.javaws.exceptions.ExitException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Yepez Hinostroza on 19/06/2016.
+ */
+public class TemperaturaEstable {
+    @Test
+    public void main() throws Exception {
+        BeerFridgeModel beerFridgemodel= BeerFridgeModel.getInstance();
+        ControllerInterface controller = new BeerFridgeController(beerFridgemodel);
+        int diferencia=Math.abs(beerFridgemodel.getTempDeseada()-beerFridgemodel.tempActual);
+        Diferenciador diferenciador= new Diferenciador(diferencia);
+        diferenciador.hilo.start();
+        diferenciador.hilo.join();
+
+        if(Math.abs(beerFridgemodel.getTempDeseada()-beerFridgemodel.tempActual)>5)
+        {
+            throw new Exception("Demora mucho en alcanzar la temp Deseada");
+        }
+
+
+
+    }
+
+}
