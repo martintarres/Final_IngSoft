b7dc8d6fef955d93c02b740f98049a8d57e078c9
Martin <martintarres@gmail.com>
2016-06-23 19:13:24 -0300

    arreglando problemas en los test

--- src/test/java/headfirst/combined/djview/BeerFridgeControllerTest.java
+++ src/test/java/headfirst/combined/djview/BeerFridgeControllerTest.java
@@ -2,19 +2,20 @@ package headfirst.combined.djview;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.*;
 import static org.junit.Assert.*;
 /**
  * Created by martin on 23/06/2016.
  */
 public class BeerFridgeControllerTest {
+
     @Test
     public void increasetemp() throws Exception {
         BeerFridgeModel beerFridgeModel = BeerFridgeModel.getInstance();
         BeerFridgeController controlador = new BeerFridgeController(beerFridgeModel);
-        beerFridgeModel.setTempDeseada(4);
+        beerFridgeModel.setTempDeseada(2);
         controlador.increaseBPM();
-        assertEquals(5, beerFridgeModel.getTempDeseada());
+        assertEquals(3, beerFridgeModel.getTempDeseada());
     }
 
     @Test
@@ -23,7 +24,7 @@ public class BeerFridgeControllerTest {
         BeerFridgeController controlador = new BeerFridgeController(beerFridgeModel);
         beerFridgeModel.setTempDeseada(3);
         controlador.decreaseBPM();
-        assertEquals(4, beerFridgeModel.getTempDeseada());
+        assertEquals(2, beerFridgeModel.getTempDeseada());
     }
 
     @Test
@@ -32,7 +33,7 @@ public class BeerFridgeControllerTest {
         BeerFridgeController controlador = new BeerFridgeController(beerFridgeModel);
         beerFridgeModel.setTempDeseada(-1);
         controlador.decreaseBPM();
-        assertEquals(0, beerFridgeModel.getTempDeseada());
+        assertEquals(-2, beerFridgeModel.getTempDeseada());
     }
 
     @Test
